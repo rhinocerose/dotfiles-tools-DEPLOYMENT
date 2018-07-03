564f3bf2f1206be9851844e76323eaabcecac572
ps1337 <ps1337@mailbox.org>
2018-07-04 00:25:24 +0200

    fix file manager start script

--- i3/scripts/launchFilemanager.sh
+++ i3/scripts/launchFilemanager.sh
@@ -7,9 +7,20 @@ count=$(pgrep -fc "$FILEMANAGER")
 
 # If theres an instance already
 if [ "$count" -gt 0 ]; then
-    i3-msg "[class=$FILEMANAGERWINDOWCLASS] focus"
+    i3-msg "[title=$FILEMANAGERWINDOWNAME] focus"
 # None exists, start a new one
 else
-	# shellcheck disable=SC2091
-    $(env "$FILEMANAGER")
+    exec termite -e "$FILEMANAGER"
 fi
+
+# Use this for nautilus:
+# count=$(pgrep -fc "$FILEMANAGER")
+#
+# # If theres an instance already
+# if [ "$count" -gt 0 ]; then
+#     i3-msg "[class=$FILEMANAGERWINDOWCLASS] focus"
+# # None exists, start a new one
+# else
+#         # shellcheck disable=SC2091
+#     $(env "$FILEMANAGER")
+# fi
